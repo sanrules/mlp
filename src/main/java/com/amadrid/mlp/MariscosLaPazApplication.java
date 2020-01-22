6ee37cb193d381803999f91e984b86ab8822e2a6
Sandra <sankiry93@gmail.com>
2020-01-22 22:37:42 +0100

    Initial commit: NEW PROJECT WITH SPRING BOOT

---
+++ src/main/java/com/amadrid/mlp/MariscosLaPazApplication.java
@@ -0,0 +1,12 @@
+package com.amadrid.mlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class MariscosLaPazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariscosLaPazApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amadrid/mlp/MariscosLaPazApplicationTests.java
@@ -0,0 +1,11 @@
+package com.amadrid.mlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class MariscosLaPazApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
