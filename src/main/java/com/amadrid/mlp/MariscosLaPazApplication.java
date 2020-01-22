59b20ab374e9eefd097dc7e90c52a84d86061b85
Sandra <sankiry93@gmail.com>
2020-01-22 22:46:50 +0100

    Prueba commit usuario AMADRID

--- src/main/java/com/amadrid/mlp/MariscosLaPazApplication.java
+++ src/main/java/com/amadrid/mlp/MariscosLaPazApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication public class MariscosLaPazApplication {
 
+    private final static String prueba = "Prueba";
+
     public static void main(String[] args) {
         SpringApplication.run(MariscosLaPazApplication.class, args);
     }
